7a9e5b483ea570ebc7860c36c3a1a98350a5d914
Shubham <shubhampwr9945@gmail.com>
2023-08-27 17:31:21 +0530

    rendering expense acc to year change

--- src/component/Expenses/Expense.js
+++ src/component/Expenses/Expense.js
@@ -3,36 +3,34 @@ import ExpenseItem from "./ExpenseItem";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expense.css";
 const Expense = (props) => {
-  //   const [filteredDate, setFilteredDate] = useState("2019");
+  const [filteredDate, setFilteredDate] = useState("2019");
   //   const [specYearExpense, setSpecYearExpense] = useState([
   //     // ...props.item.filter((ele) => {
   //     //   return ele.date.getFullYear() === Number(filteredDate);
   //     // }),
   //   ]);
 
-  const [expenData, setExpenData] = useState({
-    filteredDate: "2019",
-    specYearExpense: [
-      ...props.item.filter((ele) => {
-        console.log(Number(ele.date.getFullYear()), "in");
-        return Number(ele.date.getFullYear()) === this.filteredDate;
-      }),
-    ],
+  const filteredArr = props.item.filter((ele) => {
+    return ele.date.getFullYear().toString() === filteredDate;
   });
 
+  // const [ set = useState({
+  //   filteredDate: "2019",
+  //   specYearExpense: [...filteredArr],
+  // });
+
   const yearChangeHandler = (newYear) => {
-    setExpenData((prevState) => {
-      return {
-        filteredDate: newYear,
-        specYearExpense: [
-          ...props.item.filter((ele) => {
-            console.log(Number(ele.date.getFullYear()), "in");
-            return Number(ele.date.getFullYear()) === this.filteredDate;
-          }),
-        ],
-      };
-    });
-    // setFilteredDate(newYear);
+    // set(prevState) => {
+    //   return {
+    //     filteredDate: newYear,
+    //     specYearExpense: [
+    //       props.item.filter((ele) => {
+    //         return ele.date.getFullYear().toString() === newYear;
+    //       }),
+    //     ],
+    //   };
+    // });
+    setFilteredDate(newYear);
     // setSpecYearExpense([
     //   ...props.item.filter((ele) => {
     //     console.log(Number(ele.date.getFullYear()), "in");
@@ -41,18 +39,15 @@ const Expense = (props) => {
     // ]);
   };
 
-  console.log(expenData.filteredDate, "filter");
-  console.log(expenData.specYearExpense, "data");
-
   return (
     <>
       <div className="filterContainer">
         <ExpenseFilter
-          selected={expenData.filteredDate}
+          selected={filteredDate}
           onYearChange={yearChangeHandler}
         />
       </div>
-      {props.item.map((ele, ind) => (
+      {filteredArr.map((ele, ind) => (
         <ExpenseItem key={ind} expense={ele} />
       ))}
     </>
